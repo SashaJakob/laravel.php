4779fcdc33153815ab668cef12c943d2f165bdab
unknown <sashayakub8@gmail.com>
2023-07-24 23:44:05 +0300

    commit

---
+++ app/ Http/ Controllers/ ProductController.php
@@ -0,0 +1,291 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\ProductImage;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+use Validator;
+class ProductController extends Controller
+{
+    /**
+     * @OA\Get(
+     *     tags={"Product"},
+     *     path="/api/product",
+     *     @OA\Response(response="200", description="Get All Products.")
+     * )
+     */
+    public function index()
+    {
+        $products = Product::with('product_images')->get();
+
+        return response()->json($products, 200, [
+            'Content-Type' => 'application/json;charset=UTF-8',
+            'Charset' => 'utf-8'
+        ], JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * @OA\Post(
+     *     tags={"Product"},
+     *     path="/api/product",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 required={"category_id","name","price","description","images[]"},
+     *                 @OA\Property(
+     *                     property="category_id",
+     *                     type="integer"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="price",
+     *                     type="number"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="description",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="images[]",
+     *                     type="array",
+     *                     @OA\Items(type="string", format="binary")
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Add Product.")
+     * )
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'category_id' => 'required|exists:categories,id',
+            'name' => 'required',
+            'price' => 'required',
+            'description' => 'required',
+            'images' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+        $product = Product::create($input);
+
+        if ($request->hasFile('images')) {
+            $images = $request->file('images');
+            $i =1;
+            foreach ($images as  $image) {
+                $filename = uniqid() . '.' . $image->getClientOriginalExtension();
+                $priority = $i++;
+                $image->move(public_path('uploads/product'), $filename);
+                ProductImage::create([
+                    'product_id' => $product->id,
+                    'name' => $filename,
+                    'priority' => $priority,
+                ]);
+            }
+        }
+
+        $product->load('product_images');
+
+        return response()->json($product, 200, [
+            'Content-Type' => 'application/json;charset=UTF-8',
+            'Charset' => 'utf-8'
+        ], JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * @OA\Get(
+     *     tags={"Product"},
+     *     path="/api/product/{id}",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Product ID",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(response="200", description="Get a Product.")
+     * )
+     */
+    public function getByid($id)
+    {
+        $product = Product::with('product_images')->find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404, [
+                'Content-Type' => 'application/json;charset=UTF-8',
+                'Charset' => 'utf-8'
+            ], JSON_UNESCAPED_UNICODE);
+        }
+
+        return response()->json($product, 200, [
+            'Content-Type' => 'application/json;charset=UTF-8',
+            'Charset' => 'utf-8'
+        ], JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * @OA\Post(
+     *     tags={"Product"},
+     *     path="/api/product/edit/{id}",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Product ID",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 required={"category_id","name","price","description","images[]"},
+     *                 @OA\Property(
+     *                     property="category_id",
+     *                     type="integer"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="price",
+     *                     type="number"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="description",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="images[]",
+     *                     type="array",
+     *                     @OA\Items(type="string", format="binary")
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Update a Product.")
+     * )
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::with('product_images')->find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404, [
+                'Content-Type' => 'application/json;charset=UTF-8',
+                'Charset' => 'utf-8'
+            ], JSON_UNESCAPED_UNICODE);
+        }
+
+        $input = $request->all();
+        $message = [
+            'category_id.required' => "Specify the category",
+            'name.required' => "Specify the name",
+            'price.required' => "Specify the price",
+            'description.required' => "Specify the description",
+            'images.required' => "Select at least one image",
+        ];
+
+        $validator = Validator::make($input, [
+            'category_id' => 'required|exists:categories,id',
+            'name' => 'required',
+            'price' => 'required',
+            'description' => 'required',
+            'images' => 'required|array',
+            'images.*' => 'image|mimes:jpeg,png,jpg',
+        ], $message);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400, [
+                'Content-Type' => 'application/json;charset=UTF-8',
+                'Charset' => 'utf-8'
+            ], JSON_UNESCAPED_UNICODE);
+        }
+
+        $productImages = $product->product_images;
+        foreach ($productImages as $productImage) {
+            // Delete the file
+            $filePath = public_path('uploads/product/' . $productImage->name);
+            if (File::exists($filePath)) {
+                File::delete($filePath);
+            }
+        }
+
+        $product->update($input);
+
+        if ($request->hasFile('images')) {
+            $images = $request->file('images');
+            $i =1;
+            foreach ($images as  $image) {
+                $filename = uniqid() . '.' . $image->getClientOriginalExtension();
+                $priority = $i++;
+                $image->move(public_path('uploads/product'), $filename);
+                $productImage = ProductImage::updateOrCreate(
+                    ['product_id' => $product->id, 'priority' => $priority],
+                    ['name' => $filename]
+                );
+            }
+        }
+
+        $product->load('category', 'product_images');
+
+        return response()->json($product, 200, [
+            'Content-Type' => 'application/json;charset=UTF-8',
+            'Charset' => 'utf-8'
+        ], JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * @OA\Delete(
+     *     tags={"Product"},
+     *     path="/api/product/{id}",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Product ID",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(response="204", description="Delete a Product.")
+     * )
+     */
+    public function delete($id)
+    {
+        $product = Product::with('product_images')->find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404, [
+                'Content-Type' => 'application/json;charset=UTF-8',
+                'Charset' => 'utf-8'
+            ], JSON_UNESCAPED_UNICODE);
+        }
+
+        $productImages = $product->product_images;
+        foreach ($productImages as $productImage) {
+            // Delete the file
+            $filePath = public_path('uploads/product/' . $productImage->name);
+            if (File::exists($filePath)) {
+                File::delete($filePath);
+            }
+        }
+
+        $product->product_images()->delete();
+        $product->delete();
+
+        return response()->json(null, 204, [
+            'Content-Type' => 'application/json;charset=UTF-8',
+            'Charset' => 'utf-8'
+        ], JSON_UNESCAPED_UNICODE);
+    }
+
+}
---
+++ app/ Http/ Controllers/CategoryController.php
@@ -0,0 +1,266 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
+use Validator;
+
+class CategoryController extends Controller
+{
+    /**
+     * @OA\Get(
+     *     tags={"Category"},
+     *     path="/api/category",
+     *     @OA\Response(response="200", description="List Categories.")
+     * )
+     */
+    public function index()
+    {
+        $list = Category::all();
+        return response()->json($list, 200,
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * @OA\Get(
+     *     tags={"Category"},
+     *     path="/api/category/{id}",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Ідентифікатор категорії",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="number",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\Response(response="200",
+     *      description="List Categories."
+     *      ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Категорії не знайдено"
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Не авторизований"
+     *     )
+     * )
+     */
+    public function getById($id)
+    {
+        $category = Category::findorFail($id);
+
+        if (!$category) {
+            return response()->json(['message' => 'Category not found'], 404);
+        }
+
+        return response()->json($category, 200,
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
+    }
+
+    /**
+     * @OA\Post(
+     *     tags={"Category"},
+     *     path="/api/category",
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 required={"name","image","description"},
+     *                 @OA\Property(
+     *                     property="image",
+     *                     type="file",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="description",
+     *                     type="string"
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Add Category.")
+     * )
+     */
+    public function store(Request $request) {
+        $input = $request->all();
+        $message = array(
+            'name.required'=>"Вкажіть назву категорії",
+            'image.required'=>"Вкажіть фото категорії",
+            'description.required'=>"Вкажіть опис категорії",
+        );
+        $validator = Validator::make($input,[
+            'name'=>'required',
+            'image'=>'required',
+            'description'=>'required'
+        ], $message);
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 400,
+                ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
+        }
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            // Generate a unique filename
+            $filename = uniqid() . '.' . $image->getClientOriginalExtension();
+            $sizes = [50, 150, 300, 600, 1200];
+            foreach ($sizes as $size)
+            {
+                $fileSave = $size.'_'.$filename;
+                // Resize the image while maintaining aspect ratio
+                $resizedImage = Image::make($image)->resize($size, null, function ($constraint) {
+                    $constraint->aspectRatio();
+                })->encode();
+                // Save the resized image
+                $path = public_path('uploads/' . $fileSave);
+                file_put_contents($path, $resizedImage);
+            }
+            $input['image'] = $filename;
+        }
+        $category = Category::create($input);
+        return response()->json($category, 200,
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
+    }
+
+
+    /**
+     * @OA\Post(
+     *     tags={"Category"},
+     *     path="/api/category/edit/{id}",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Ідентифікатор категорії",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="number",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *         @OA\MediaType(
+     *             mediaType="multipart/form-data",
+     *             @OA\Schema(
+     *                 required={"name"},
+     *                 @OA\Property(
+     *                     property="image",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="description",
+     *                     type="string"
+     *                 )
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(response="200", description="Add Category.")
+     * )
+     */
+    public function update($id, Request $request) {
+        $category = Category::findOrFail($id);
+        $input = $request->all();
+        $message = array(
+            'name.required'=>"Вкажіть назву категорії",
+            'description.required'=>"Вкажіть опис категорії",
+        );
+        $validator = Validator::make($input,[
+            'name'=>'required',
+            'description'=>'required'
+        ], $message);
+        if($validator->fails()) {
+            return response()->json($validator->errors(), 400,
+                ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
+        }
+
+        if ($request->hasFile('image')) {
+            $image = $request->file('image');
+            // Generate a unique filename
+            $filename = uniqid() . '.' . $image->getClientOriginalExtension();
+            $sizes = [50, 150, 300, 600, 1200];
+            //remove old images
+            foreach ($sizes as $size) {
+                $fileDelete = $size.'_'.$category->image;
+                $removePath = public_path('uploads/' . $fileDelete);
+                if (file_exists($removePath)) {
+                    unlink($removePath);
+                }
+            }
+
+            foreach ($sizes as $size)
+            {
+                $fileSave = $size.'_'.$filename;
+                // Resize the image while maintaining aspect ratio
+                $resizedImage = Image::make($image)->resize($size, null, function ($constraint) {
+                    $constraint->aspectRatio();
+                })->encode();
+                // Save the resized image
+                $path = public_path('uploads/' . $fileSave);
+                file_put_contents($path, $resizedImage);
+            }
+            $input['image'] = $filename;
+        }
+        else {
+            $input['image'] = $category->image;
+        }
+
+        $category->update($input);
+        return response()->json($category, 200,
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
+    }
+
+
+    /**
+     * @OA\Delete(
+     *     path="/api/category/{id}",
+     *     tags={"Category"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="Ідентифікатор категорії",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="number",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Успішне видалення категорії"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Категорії не знайдено"
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Не авторизований"
+     *     )
+     * )
+     */
+    public function delete(Request $request, $id) {
+        $category = Category::findOrFail($id);
+        $sizes = [50, 150, 300, 600, 1200];
+        //remove old images
+        foreach ($sizes as $size) {
+            $fileDelete = $size.'_'.$category->image;
+            $removePath = public_path('uploads/' . $fileDelete);
+            if (file_exists($removePath)) {
+                unlink($removePath);
+            }
+        }
+        $category->delete();
+        return 204;
+    }
+}
